3c757f103f26d19e6f75ea75eb4a911dbb176821
LuHao <928186846@qq.com>
2020-11-30 10:19:30 +0800

    手机工具箱 V1.0.0 1，股市金字塔计算器 2，计算器 3，二维码

--- pyramid/src/main/java/com/wwxd/toolkit/pyramid/fragment/PyramidFragment.kt
+++ pyramid/src/main/java/com/wwxd/toolkit/pyramid/fragment/PyramidFragment.kt
@@ -4,9 +4,11 @@ import android.text.TextUtils
 import android.view.KeyEvent
 import android.view.View
 import android.widget.TextView
+import com.wwdx.toolkit.utils.ToastUtil
 import com.wwxd.toolkit.base.BaseFragment
 import com.wwxd.toolkit.pyramid.R
 import kotlinx.android.synthetic.main.fragment_pyramid.*
+import java.lang.Exception
 
 /**
  * user：LuHao
@@ -14,8 +16,7 @@ import kotlinx.android.synthetic.main.fragment_pyramid.*
  * describe：金字塔
  */
 class PyramidFragment : BaseFragment() {
-    private val defPhaseNum = 10
-    private val defConnectStr = "     -->     "
+    private val defPhaseNum = 20
     private val startValue = 0.5//阶段分割，0.5个百分点
     override fun setContentView(): Int {
         return R.layout.fragment_pyramid
@@ -28,33 +29,39 @@ class PyramidFragment : BaseFragment() {
                 && KeyEvent.ACTION_DOWN == event.getAction()
                 && !TextUtils.isEmpty(etPrice.text)
             ) {
-                updatePyramid(etPrice.text.toString().toDouble())
+                updatePyramid()
                 true
             }
             false
         }
+        btnSelect.setOnClickListener { updatePyramid() }
     }
 
     //更新开盘价的金字塔价格
-    private fun updatePyramid(openingPrice: Double) {
-        llPyramidBuy.removeAllViews()
-        llPyramidSell.removeAllViews()
-        for (i in 0 until defPhaseNum) {
-            val itemBuyView = View.inflate(context, R.layout.item_pyramid_buy, null)
-            val textBuyView = itemBuyView.findViewById<TextView>(R.id.textPyramidBuy)
-            textBuyView.text = StringBuilder()
-                .append(upBuyOrSellPercentage(i, true))
-                .append(defConnectStr)
-                .append(upBuyOrSellValue(i, openingPrice, true))
-            llPyramidBuy.addView(itemBuyView)
-
-            val itemSellView = View.inflate(context, R.layout.item_pyramid_sell, null)
-            val textSellView = itemSellView.findViewById<TextView>(R.id.textPyramidSell)
-            textSellView.text = StringBuilder()
-                .append(upBuyOrSellPercentage(i, false))
-                .append(defConnectStr)
-                .append(upBuyOrSellValue(i, openingPrice, false))
-            llPyramidSell.addView(itemSellView)
+    private fun updatePyramid() {
+        if (TextUtils.isEmpty(etPrice.text)) return
+        try {
+            val openingPrice = etPrice.text.toString().toDouble()
+            hideSoftInputFromWindow(etPrice)
+            llPyramidBuy.removeAllViews()
+            llPyramidSell.removeAllViews()
+            for (i in 0 until defPhaseNum) {
+                val itemBuyView = View.inflate(context, R.layout.item_pyramid_buy, null)
+                val textPyramidBuy1 = itemBuyView.findViewById<TextView>(R.id.textPyramidBuy1)
+                val textPyramidBuy2 = itemBuyView.findViewById<TextView>(R.id.textPyramidBuy2)
+                textPyramidBuy1.text = upBuyOrSellPercentage(i, true)
+                textPyramidBuy2.text = upBuyOrSellValue(i, openingPrice, true)
+                llPyramidBuy.addView(itemBuyView)
+                val itemSellView = View.inflate(context, R.layout.item_pyramid_sell, null)
+                val textPyramidSell1 = itemSellView.findViewById<TextView>(R.id.textPyramidSell1)
+                val textPyramidSell2 = itemSellView.findViewById<TextView>(R.id.textPyramidSell2)
+                textPyramidSell1.text = upBuyOrSellPercentage(i, false)
+                textPyramidSell2.text = upBuyOrSellValue(i, openingPrice, false)
+                llPyramidSell.addView(itemSellView)
+            }
+        } catch (e: Exception) {
+            etPrice.setText("")
+            ToastUtil.showFailureToast(getString(R.string.str_price_error))
         }
     }
 
